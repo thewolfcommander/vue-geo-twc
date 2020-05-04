f95173c244dd08a2027781cbca49288ba15d0c3a
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-05-04 22:50:09 +0530

    Initialized firebase

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <div class="navbar">
+        <nav class="deep-purple darken-1">
+            <div class="container">
+                <span class="brand-logo left">
+                    <router-link to="">
+                        GeoNinjas
+                    </router-link>
+                </span>
+                <ul class="right">
+                    <li>
+                        <router-link>Signup</router-link>
+                    </li>
+                    <li>
+                        <router-link>Login</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+
+
+</style>
